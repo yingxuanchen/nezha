4e5f20f8fb3f744b4ce4037846673782c5755eb6
Chen Yingxuan <yingxuanchen@hotmail.com>
2025-04-30 14:41:15 +0800

    setup backend cors for prod

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,25 @@
 import express from "express";
 import { MongoClient } from "mongodb";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+
+const allowedOrigins = ["http://localhost:5173", "https://yingxuanchen.github.io"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like curl or Postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.some((allowed) => origin.startsWith(allowed))) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
 const client = new MongoClient(process.env.MONGO_URI);
 let db;
